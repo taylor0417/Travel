19a60f92ebf658b53e5fa7fcb2429bb42f158f2d
taylor0417 <taylor57568@163.com>
2018-08-02 14:50:50 +0800

    index finish

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,7 +2,7 @@
   <div class="">
     <div class="recommend-title"><img class="heart" src="http://img1.qunarzz.com/piao/fusion/1711/89/ebc329f16c55bb02.png" alt="">猜你喜欢</div>
     <ul>
-      <li class="item" v-for="item of recommend" :key="item.id">
+      <li class="item" v-for="item of list" :key="item.id">
         <div class="img-wrapper">
           <span class="tips">购买随用</span>
           <img class="item-img" :src="item.imgUrl" alt="">
@@ -34,66 +34,8 @@
 <script>
 export default {
   name: 'HomeRecommend',
-  data () {
-    return {
-      recommend: [{
-        id: '0001',
-        title: '北京国际鲜花港',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1803/12/126471e0966aca5ba3.img.jpg_200x200_f1286016.jpg',
-        comment: 1930,
-        price: 19.9,
-        location: '顺义区'
-      }, {
-        id: '0002',
-        title: '北京欢乐谷',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/a5/4003f9dd7bebf61eccbf64046e26d487.water.jpg_200x200_7690e4cd.jpg',
-        comment: 55871,
-        price: 62,
-        location: '朝阳区'
-      }, {
-        id: '0003',
-        title: '故宫',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg',
-        comment: 252407,
-        price: 20,
-        location: '东城区'
-      }, {
-        id: '0004',
-        title: '北京欢乐水魔方',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/201405/23/0265195fc4b7913a758674357c3cc8f6.jpg_200x200_b58ea00b.jpg',
-        comment: 2941,
-        price: 111,
-        location: '丰台区'
-      }, {
-        id: '0005',
-        title: '水乐方嬉水乐园',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1501/c2/c212a154649088c1.water.jpg_200x200_69d1df69.jpg',
-        comment: 1630,
-        price: 228.5,
-        location: '水立方'
-      }, {
-        id: '0006',
-        title: '古北水镇',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1804/c8/c88bb5e69c9a9c5ea3.img.jpg_200x200_740720ff.jpg',
-        comment: 7070,
-        price: 150,
-        location: '密云县'
-      }, {
-        id: '0007',
-        title: '北京野生动物园',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/36/ce3d2d6c9ab44d67ae68d940b8781829.water.jpg_200x200_0938a8f2.jpg',
-        comment: 11834,
-        price: 71,
-        location: '大兴区'
-      }, {
-        id: '0008',
-        title: '北京杜莎夫人蜡像馆',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1608/4f/4fc3f5a1b838f279a3.water.jpg_200x200_e6528ef6.jpg',
-        comment: 6473,
-        price: 85,
-        location: '前门大街'
-      }]
-    }
+  props: {
+    list: Array
   }
 }
 </script>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption">
-      <swiper-slide v-for="item of swiperList" :key="item.id">
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <swiper-slide v-for="item of list" :key="item.id">
         <img class="swiper-img" :src="item.imgUrl" alt="">
       </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
@@ -12,25 +12,20 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true
-      },
-      swiperList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/fa/800bd7f554abf302.jpg_750x200_9d809aad.jpg'
-      }, {
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/e7/d70d0830a9941b02.jpg_750x200_f8c7d2ad.jpg'
-      }, {
-        id: '0003',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1707/90/2ab6fd356529aa02.jpg_750x200_4120afc5.jpg'
-      }, {
-        id: '0004',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/66/e5a5cec881702f02.jpg_750x200_67bb5691.jpg'
-      }]
+      }
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
     }
   }
 }
