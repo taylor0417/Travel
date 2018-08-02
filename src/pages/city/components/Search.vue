7287732d84ac564b1f5c69fbc772cfad92d2f017
taylor0417 <taylor57568@163.com>
2018-08-02 23:50:06 +0800

    search finish

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,65 @@
 <template>
-  <div class="search">
-    <input class="searchInput" type="text" name="" value="" placeholder="输入城市名或拼音">
+  <div class="">
+    <div class="search">
+      <input v-model="keyword" class="searchInput" type="text" name="" value="" placeholder="输入城市名或拼音">
+    </div>
+    <div
+        class="search-content"
+        ref="search"
+        v-show="keyword"
+    >
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -25,5 +78,19 @@ export default {
       text-align: center
       border-radius: .06rem
       width: 100%
+      color: #666
+  .search-content
+    position: absolute
+    top: 1.4rem
+    left: 0
+    right: 0
+    bottom: 0
+    overflow: hidden
+    background: #eee
+    z-index: 1
+    .search-item
+      line-height: .62rem
+      padding-left: .2rem
+      background: #fff
       color: #666
 </style>
